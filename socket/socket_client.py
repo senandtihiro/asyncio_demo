073b7aae947f157fdd09328ea28dd7b525d2ae26
senandtihiro <yulian_he@126.com>
2019-02-28 10:05:35 +0800

    实现简单的聊天功能

--- socket/socket_client.py
+++ socket/socket_client.py
@@ -13,8 +13,15 @@ close()
 '''
 import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 8000))
-client.send('bobby'.encode('utf8'))
-data = client.recv(1024)
-print('接收到来自server的数据:', data.decode('utf8'))
-client.close()
+client.connect(('127.0.0.1', 8001))
+
+# client.send('bobby'.encode('utf8'))
+# data = client.recv(1024)
+# print('接收到来自server的数据:', data.decode('utf8'))
+# client.close()
+
+while True:
+    re_data = input()
+    client.send(re_data.encode('utf8'))
+    data = client.recv(1024)
+    print('接收到来自server的数据:', data.decode('utf8'))
--- socket/socket_server.py
+++ socket/socket_server.py
@@ -17,16 +17,23 @@ close()
 import socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('0.0.0.0', 8000))
+server.bind(('0.0.0.0', 8001))
 server.listen()
 sock, addr = server.accept()
 
 # 获取从客户端发送的数据
 # 一次获取1K的数据
-data = sock.recv(1024)
-print(data.decode('utf8'))
-# send的是client发送过来的数据，所以要先解码，
-# 而发送出去的时候要求是二进制数据，因此要再进行编码
-sock.send('hello {}'.format(data.decode('utf8')).encode('utf8'))
-server.close()
-sock.close()
+# data = sock.recv(1024)
+# print(data.decode('utf8'))
+# # send的是client发送过来的数据，所以要先解码，
+# # 而发送出去的时候要求是二进制数据，因此要再进行编码
+# sock.send('hello {}'.format(data.decode('utf8')).encode('utf8'))
+# server.close()
+# sock.close()
+
+# 在此基础上实现初级的聊天功能
+while True:
+    data = sock.recv(1024)
+    print('接收到来自client的数据：', data.decode('utf8'))
+    re_data = input()
+    sock.send(re_data.encode('utf8'))
