129f8beac6f0776cec91a728a9f9233a4bad1dc5
senandtihiro <yulian_he@126.com>
2019-02-28 10:13:54 +0800

    用多线程处理多个client的连接

--- socket/socket_server.py
+++ socket/socket_server.py
@@ -13,13 +13,13 @@ send()
 
 close()
 '''
-
+import threading
 import socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('0.0.0.0', 8001))
 server.listen()
-sock, addr = server.accept()
+# sock, addr = server.accept()
 
 # 获取从客户端发送的数据
 # 一次获取1K的数据
@@ -31,9 +31,23 @@ sock, addr = server.accept()
 # server.close()
 # sock.close()
 
-# 在此基础上实现初级的聊天功能
-while True:
+
+def handle_sock(sock, addr):
+    '''
+    用于接收用户的输入，同时发送新的消息给client
+    :param sock:
+    :param addr:
+    :return:
+    '''
     data = sock.recv(1024)
     print('接收到来自client的数据：', data.decode('utf8'))
     re_data = input()
     sock.send(re_data.encode('utf8'))
+
+
+# 在此基础上实现初级的聊天功能
+while True:
+    sock, addr = server.accept()
+    # 用线程去处理新接收的连接（用户）
+    client_thread = threading.Thread(target=handle_sock, args=(sock, addr))
+    client_thread.start()
